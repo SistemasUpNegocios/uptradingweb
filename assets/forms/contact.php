1b5aea02dad507072da04ef95cf90d8909d487d4
PaolaRosales <paolarosalesv2001@gmail.com>
2023-01-23 11:54:25 -0600

    Up Trading Web

---
+++ assets/forms/contact.php
@@ -0,0 +1,21 @@
+<?php
+	
+	include('conn.php');
+	$nombre = $_POST['nombre'];
+	$correo = $_POST['correo'];
+	$asunto = $_POST['asunto'];
+	$msj = $_POST['msj'];	
+
+	$sql = "INSERT INTO contacto (nombre, correo, asunto, msj) VALUES('$nombre', '$correo',  '$asunto','$msj')";
+	$res = mysqli_query($conn, $sql);
+
+	if ($res)
+	{
+		//echo 'Msj enviado';
+	}
+	else
+	{
+		echo mysqli_error($conn);
+	}
+
+?>
---
+++ assets/js/whats.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('#WAButton').floatingWhatsApp({
+        phone: '+5261836459734',
+        popupMessage: 'Hola, gracias por confiar en Up Trading Experts, ¿En qué podemos ayudarte?',
+        showPopup: true,
+        showOnIE: true,
+        headerColor: '#00829B',
+        backgroundColor: '#01bbcc',
+        headerTitle: 'UP Trading Experts',
+        position: "right",
+        buttonImage: '<img class="contact-whats" src="assets/images/whatsicon.png">',
+        size: '50px'
+    });
+});
